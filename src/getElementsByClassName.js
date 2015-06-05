49858490c69ddb1d518c7e4d6404c67b77924321
Dave Schumaker <daveschumaker@gmail.com>
2015-06-04 18:53:55 -0700

    getElementsByClassName is now working after much swearing, stomping around, and hair pulling.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,27 +6,27 @@
 // But instead we're going to implement it from scratch:
 
   var getElementsByClassName = function(className){
-    // your code here
-
-    // Array that we'll use to temporarily store any elements we find that match our search criteria.
-    var elementArray = [];
-
-    // Begin traversing the DOM tree by going through every single element on the page using
-    // jQuery's .each() function. I feel like this is kind of cheating and may need to go back and
-    // rewrite this.
-    $('*').each(function(index, value) {
-      // Check if the className contains the string. Using contains rather than is equal to since some
-      // HTML classes can have more than one class associated with them.
-      var getClasses = $(this).attr('class');
-      //console.log(getClasses);
-      if (typeof getClasses != 'undefined' && getClasses.indexOf(className) != -1) {
-        //console.log("FOUND!");
-        //console.log("Valid Object Below:");
-        var testObject = $(this);
-        console.log(testObject.context);
-        console.log($(this));
+    // Function that we can use to iterate through all nodes in an HTML document.
+    var nodeIterator = function(node) {
+      if (node.classList.contains(className)) {
+        nodeArray.push(node); // Push node into our nodeArray for later use.
       }
-      
-    });
+
+      // Iterate through our node.
+      // Initially, I was using node.childNodes and node.childNodes.hasChildNodes() which was giving me all sorts of errors. Switched to this method and things now seem to work flawlessly. Weird, man. Really weird.
+      for (var i = 0; i < node.children.length; i++) {
+        if (node.children[i].hasChildNodes) {
+          nodeIterator(node.children[i]);
+        }
+      }
+    }  
+
+    // Array that we'll use to temporarily store any nodes we find that 
+    // match our search criteria from className.
+    var nodeArray = [];    
+
+    nodeIterator(document.body);
+    
+    return nodeArray;
 
   };
